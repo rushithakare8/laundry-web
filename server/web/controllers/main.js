85f5e3dd8ef3bf5fb86c80ef4077ab98adf0663c
Luis Gonzalez <luis.gonzalez@glassdoor.com>
2016-03-28 22:43:48 -0700

    Properly implemented boom and the catch chain, when a promise is returned with in a promise only one catch is enough

--- server/web/controllers/main.js
+++ server/web/controllers/main.js
@@ -1,4 +1,4 @@
-import Boom from 'boom';
+import boom from 'boom';
 import ViewData from '../helpers/ViewData';
 import appRender from '../helpers/appRender';
 import { getState } from '../helpers/stateCreator';
@@ -9,17 +9,13 @@ exports.index = {
     const baseData = ViewData.getBaseData();
     getState(request).then((state) => {
       baseData.state = state;
-      console.log(state);
-      appRender(request.path, state).then((html) => {
+      return appRender(request.path, state).then((html) => {
         baseData.html = html;
         return reply.view('main', baseData);
-      }).catch((err) => {
-        console.log(err);
-        reply(new Boom(err));
       });
     }).catch((err) => {
-      console.log(err);
-      reply(new Boom(err));
+      console.error(err);
+      return reply(boom.badImplementation('Internal Server Error'));
     });
   },
 };
