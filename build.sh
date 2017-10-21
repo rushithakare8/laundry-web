839d3cadb5e1372b5ad3345791faa7fa7f0bebbd
Cesar Gonzalez <cesar.e.gonzalez@gmail.com>
2017-10-20 21:26:55 -0700

    fixing prices

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#npm install --silent -g grunt-cli bower
+#npm install -g grunt
+#npm install
+
+export NODE_ENV=dev
+export VAULT='{"auth":{"facebook":{"clientId":"881601445289245","clientSecret":"8b3671fe28e22dd57b525d8cb864a2c9"}},"password":"wx8(rOqJIA^yok4aBv!(l25GjwHTP0g&","api":{"user":"user","password":"user"},"stripe":"sk_test_m1f9mBO7U4X1lDnerfKhTyK9"}'
+#ln -s /Users/cesaregb/dev/workspace_node/il-middleware-services
+#ln -s /Users/cesaregb/dev/workspace_node/il-realtime-services
+#npm run build
+npm start
+
+#rethinkdb --http-port 3080
+#npm run dev
